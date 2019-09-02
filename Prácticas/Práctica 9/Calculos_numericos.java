1d585d15f840439546ab4d2c65aa281c77a139e5
Erick <vargas.erick030997@gmail.com>
2019-09-02 11:01:21 -0500

    Update

---
+++ Prácticas/Práctica 9/Calculos_numericos.java
@@ -0,0 +1,15 @@
+class Calculos_numericos{
+    public static void main(String args[]){
+        double i = 0, MAX = 1e8;
+        System.out.println( MAX );
+        double seno = 0, coseno = 0, tangente = 0, logaritmo = 0, raizCuadrada = 0;
+        while( i < MAX ){
+            seno += Math.sin( i );
+            coseno += Math.cos( i );
+            tangente += Math.tan( i );
+            logaritmo += Math.log( i );
+            raizCuadrada += Math.sqrt( i );
+            i++;
+        }
+    }
+}
