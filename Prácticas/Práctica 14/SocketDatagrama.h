c918b463a1603088ad6877b7bf6a0442a6262809
Erick <vargas.erick030997@gmail.com>
2019-09-24 08:45:31 -0500

    Update

---
+++ Prácticas/Práctica 14/SocketDatagrama.h
@@ -0,0 +1,16 @@
+#include "PaqueteDatagrama.h"
+#include <sys/socket.h>
+#include <bits/stdc++.h>
+
+class SocketDatagrama{
+    public:
+        SocketDatagrama( int );
+        ~SocketDatagrama();
+        int recibe( PaqueteDatagrama &p );
+        int envia( PaqueteDatagrama &p );
+    
+    private:
+        struct sockaddr_in direccionLocal;
+        struct sockaddr_in direccionForanea;
+        int s;
+};
