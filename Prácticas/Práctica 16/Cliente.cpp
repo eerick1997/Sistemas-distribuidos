dde223fa3c611cb65eb6df5e9f6507eac4333df5
ArkNeo <arkf3r@gmail.com>
2019-10-07 11:20:14 -0500

    Cliente n ciclos

--- Prácticas/Práctica 16/Cliente.cpp
+++ Prácticas/Práctica 16/Cliente.cpp
@@ -3,15 +3,22 @@
 using namespace std;
 int main()
 {
-    string i = "127.0.0.1";
+    string i; // = "127.0.0.1";
+    cout << "Ingrese la IP: ";
+    cin >> i;
     char *ip = new char[i.length()];
     strcpy(ip, i.c_str());
     Solicitud sol;
-    int num[2], res;
+    int num[2], res, n;
+    cout << "Ingrese el numero de ciclos:";
+    cin >> n;
     num[0] = 125;
     num[1] = 32;
-    char *resServer = sol.doOperation(ip, 7200, 3, (char *)num);
-    memcpy(&res, resServer, sizeof(res));
-    cout << "Suma = " << res << endl;
+    for (int j = 0; j < n; j++)
+    {
+        char *resServer = sol.doOperation(ip, 7200, 3, (char *)num);
+        memcpy(&res, resServer, sizeof(res));
+        cout << "Suma = " << res << endl;
+    }
     return 0;
 }
