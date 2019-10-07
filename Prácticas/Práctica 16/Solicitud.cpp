01ed7b6c76903f778e44fac6c1f0a22e5563d896
ArkNeo <arkf3r@gmail.com>
2019-10-07 11:39:41 -0500

    Fixed

--- Prácticas/Práctica 16/Solicitud.cpp
+++ Prácticas/Práctica 16/Solicitud.cpp
@@ -9,28 +9,27 @@ Solicitud::Solicitud()
 char *Solicitud::doOperation(char *IP, int puerto, int operationId, char *arguments)
 {
     short n = 7;
-        time_t segundos = 2;
-        suseconds_t microsegundos = 500000;
+    time_t segundos = 2;
+    suseconds_t microsegundos = 500000;
 
-        struct mensaje datos;
-        datos.messageType = 1123456;
-        datos.requestId = 3343;
-        datos.operationId = suma;
-        memcpy(datos.arguments, arguments, sizeof(arguments));
-        PaqueteDatagrama paq((char *)&datos, sizeof(datos), IP, puerto);
-        
+    struct mensaje datos;
+    datos.messageType = 1123456;
+    datos.requestId = 3343;
+    datos.operationId = suma;
+    memcpy(datos.arguments, arguments, sizeof(arguments));
+    PaqueteDatagrama paq((char *)&datos, sizeof(datos), IP, puerto);
+
     int result = -1;
-    while( (n--) > 0 && result < 0){
-        result = socketlocal -> recibeTimeout( paq, segundos, microsegundos );
+    while ((n--) > 0 && result < 0)
+    {
+        fflush(stdout);
         socketlocal->envia(paq);
-        if( result > 0 )
+        result = socketlocal->recibeTimeout(paq, segundos, microsegundos);
+        if (result > 0)
             break;
     }
-    
-    socketlocal->recibe(paq);
-    memcpy(&response, paq.obtieneDatos(), paq.obtieneLongitud());
-        
 
-    
+    memcpy(&response, paq.obtieneDatos(), paq.obtieneLongitud());
+
     return response.arguments;
 }
